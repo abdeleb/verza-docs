678b2e3f9f4f6c932a9b3eb96d74052862ddc41c
zo0r <z@zo0r.me>
2023-03-28 08:44:17 -0400

    disable algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -124,7 +124,7 @@ const config = {
         darkTheme: darkCodeTheme,
       },
 
-      algolia: {
+      /* algolia: {
         // The application ID provided by Algolia
         appId: 'XDLT9MWMNF',
 
@@ -141,7 +141,7 @@ const config = {
 
         // optional: path for search page that enabled by default (`false` to disable it)
         searchPagePath: 'search',
-      },
+      },*/
     }),
 
   plugins: ['docusaurus-plugin-sass'],
